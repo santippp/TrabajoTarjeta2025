74f807434e5e8cd19d6ad3ca891c1b85f4a52723
Ignacio <ignacio.giovannetti@gmail.com>
2025-10-27 22:53:42 -0300

    Modificacion clase Boleto y respectivos para mas informacion

--- TarjetaSube/Boleto.cs
+++ TarjetaSube/Boleto.cs
@@ -4,23 +4,39 @@ namespace TarjetaSube
 {
     public class Boleto
     {
+        private static int contador = 0;
+        
+        public int Id { get; private set; }
         public DateTime Fecha { get; private set; }
-        public decimal Monto { get; private set; }
+        public string TipoTarjeta { get; private set; }
         public string LineaColectivo { get; private set; }
+        public decimal MontoAbonado { get; private set; }
         public decimal SaldoRestante { get; private set; }
 
-        public Boleto(decimal monto, string lineaColectivo, decimal saldoRestante)
+        public Boleto(decimal tarifa, string lineaColectivo, Tarjeta tarjeta)
         {
+            contador++;
+            Id = contador;
+            
             Fecha = DateTime.Now;
-            Monto = monto;
+            TipoTarjeta = tarjeta.GetType().Name;
             LineaColectivo = lineaColectivo;
-            SaldoRestante = saldoRestante;
+            SaldoRestante = tarjeta.Saldo;
+            MontoAbonado = tarifa;
+
         }
 
         public override string ToString()
         {
-            return $"Boleto - Fecha: {Fecha:dd/MM/yyyy HH:mm:ss}, Línea: {LineaColectivo}, " +
-                   $"Monto: ${Monto}, Saldo Restante: ${SaldoRestante}";
+            return $"--------------------------------------\n" +
+                   $"  BOLETO: {Id}\n" +
+                   $"  Fecha:       {Fecha:dd/MM/yyyy HH:mm:ss}\n" +
+                   $"  Línea:       {LineaColectivo}\n" +
+                   $"  Tipo:        {TipoTarjeta}\n" +
+                   $"  Abonado:     ${MontoAbonado:F2}\n" +
+                   $"  Saldo:       ${SaldoRestante:F2}\n" +
+                   $"--------------------------------------";
         }
+
     }
 }
